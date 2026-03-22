4196aa54fc7061cdc5fa963bbb106b2ec4939a6c
Shalika Neranjana <shalika4net@gmail.com>
2026-03-22 15:44:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -114,4 +114,4 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.listen(5000, () => console.log("Server running on http://localhost:5000/"));
